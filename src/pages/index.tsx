f1d2ba6d9c56ab13dfa848b76e5b7c58c07ae173
dolphinlmg <dolphinlmg@naver.com>
2023-09-22 12:13:18 +0900

    Page 작성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,14 @@
+import Input from '@/components/Input';
+import UserProfile from '@/components/UserProfile';
+import { useState } from 'react';
+
 export default function Home() {
-  return <></>;
+  const [userId, setUserId] = useState<string>('');
+
+  return (
+    <>
+      <Input onClick={(id) => setUserId(id)} />
+      {userId && <UserProfile userId={userId} />}
+    </>
+  );
 }

3c6d68245f3a04f19fdc75cec48582039ecf9eba
dolphinlmg <dolphinlmg@naver.com>
2023-09-22 12:13:11 +0900

    SWR Config 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import axios from 'axios';
+import type { AppProps } from 'next/app';
+import { SWRConfig } from 'swr';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+
+  return (
+    <SWRConfig
+      value={{
+        fetcher,
+      }}
+    >
+      <Component {...pageProps} />
+    </SWRConfig>
+  );
 }
